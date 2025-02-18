c456673534b8a348c4cda11348f8d67c805bd0dc
lxj <2907313730@qq.com>
2025-02-19 07:21:16 +0800

    11

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,17 @@
+'use client'
+
 import MainLayout from '@/app/layouts/MainLayout'
 import BlogList from '@/components/features/blog/BlogList'
+import { useAuth } from '@/contexts/AuthContext'
 
 export default function BlogPage() {
+  const { user } = useAuth()
+
   return (
     <MainLayout>
       <div className="pt-4 px-4">
         <h1 className="text-2xl font-bold mb-6">我的博文</h1>
-        <BlogList />
+        <BlogList isPersonal={true} userId={user?.userId} />
       </div>
     </MainLayout>
   )
